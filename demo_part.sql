3f3c2e0b0f898d12ed63702d266ce5372f31f63a
Piet de Visser <piet@basetide.com>
2020-06-14 13:45:41 +0200

    beautifying

--- demo_part.sql
+++ demo_part.sql
@@ -57,6 +57,8 @@ check:
  - filtered-partition operations - check+test+demo ?
  - compressed partitions, do they also use less memory ? 
 
+ - note: logfile > 100, slow creation of tables: if LOG=16M to compare to PG..
+
 test
  - date+seq idea for pk
  - volume of pk as integer, string or even timestamp, any impact ? 
@@ -159,11 +161,13 @@ drop table  t ;
 
 purge recyclebin ; 
 
+set echo on
+
 create table pt 
-( id                number ( 9,0)   
+( id                number ( 9,0)    -- the PK and partitioning-key.
 , active            varchar2 ( 1 )   -- Y/N, show small set of Active.
 , amount            number ( 10,2 )  -- supposedly some amount of money.
-, dt                date            -- some date, case we want history stuff 
+, dt                date             -- a date, case we want history stuff 
 , payload           varchar2 ( 200 ) -- some text
 , filler            varchar2 ( 750 ) -- some data to create 1K recordsize
 )
@@ -174,6 +178,11 @@ partition by range ( id )  interval ( 10000 )
   , partition pt_4 values less than ( 40000 ) 
   , partition pt_5 values less than ( 50000 ) 
   , partition pt_6 values less than ( 60000 ) ) ;
+
+set echo off
+
+prompt .
+accept hit_enter prompt 'Check the Partitioned table... '
 
 -- beware, constraint in table-def generates global index
 create unique index pt_pk on  pt ( id ) local ; 
@@ -189,7 +198,6 @@ create table t
 , filler            varchar2 ( 750 ) 
 ) ;
 
--- beware, constraint in table-def generates global index
 create unique index t_pk on  t ( id ) ; 
 
 alter table t add constraint t_pk primary key ( id ) ;
@@ -204,14 +212,14 @@ set timing on
 
 -- fill with deliberately funny, compressiable data
 insert into pt
-select trunc ( rownum )                        -- sequene...
-,  decode ( mod ( rownum+1, 10000), 0, 'Y', 'N' )  -- every 1/1000 active=Y
-,  mod ( rownum, 10000 ) / 100                 -- 0-100, two decimals
+select trunc ( rownum -1)                        -- sequene...
+,  decode ( mod ( rownum, 10000), 0, 'Y', 'N' )  -- every 1/1000 active=Y
+,  mod ( rownum-1, 10000 ) / 100                 -- 0-100, two decimals
 ,  (sysdate - rownum )                         -- some date
 ,  rpad ( to_char (to_date ( trunc ( rownum ), 'J'), 'JSP' ), 198)
 ,  rpad ( ' ', 750 ) 
 from dual
-connect by rownum < 60000 ;
+connect by rownum <= 60000 ;
 
 commit ; 
 
@@ -221,59 +229,33 @@ insert into t select * from pt ;
 commit ;
 
 set echo off
-
-commit ;
+set timing off
 
 set echo on
 
-EXEC DBMS_STATS.gather_table_stats(user, 'PT');
-EXEC DBMS_STATS.gather_table_stats(user, 'T');
+EXEC DBMS_STATS.gather_table_stats(user, 'PT', null, null);
+EXEC DBMS_STATS.gather_table_stats(user, 'T' , null, null);
 
 set echo off
-
-set autotrace on stat 
-set timing on
-
--- how long to delete...
--- delete from pt where id < 10001 ;
-
--- so easy to remove a parittion, any partition:
-set echo on
-
--- also measure these deletes with normal-tx-delete
--- alter table pt drop partition pt_1 ; 
--- alter table pt drop partition pt_2 ; 
-
-set echo off
-set autotrace off
 
 column table_name format A20  
 column part_name  format A20 
 column hv format 999999 head High_val
-
 
 select table_name, partition_name part_name, num_rows 
 from user_tab_partitions
 where table_name like 'PT%'
 order by table_name, partition_name ; 
 
-select table_name, partition_name part_name, num_rows 
-from user_tab_partitions
-where table_name like 'T%'
-order by table_name, partition_name ; 
+select table_name, num_rows 
+from user_tables
+where table_name like 'T'
+order by table_name ; 
 
-/*
--- find  out which partition contains the values below 2000
-select table_name, partition_name part_name, high_value hv
-from user_tab_partitions 
-where table_name = 'PT'
-and 1=1
-/
-
-*/
-
-
--- what kind of key ? 
--- compare sys_guid to number to char..
+prompt .
+prompt Check: 
+prompt We have tables T (conventional) and PT (6 partitions).
+prompt With 60000 rows each.
+prompt .
 
 
