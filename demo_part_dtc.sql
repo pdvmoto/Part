59c9da78ee12dada44602e14c9a788153815ce8e
pdvbv macbook <piet.devisser@cheerful.com>
2021-04-25 16:03:05 +0200

    partdtc

--- demo_part_dtc.sql
+++ demo_part_dtc.sql
@@ -7,7 +7,7 @@
 
 Cases to examine:
  - the funny indexes pk (id, dtc), pk+lvl, pk+dtm
- - global and local indexes
+ - global and local indexes (proof of...)
  - which field in pk to put first ?
  - partion by dtc: does field order in pk matter ?
  - what if the BLOB is several block in size
@@ -22,15 +22,26 @@ create        index t1_idx_lvl  on t1 ( id, dtc, lvl ) ;
 
 Every stmnt seems to have ...where id=3113, 
 plus, expect high freq of ins/upd on this data. 
+
 And we may want to partition monthly on dtc (date_created). 
 
+notes on indexes:
+
+note that those indexes would only make sense if a range or all of the 3rd field needs to be selected. The AWR seems to show only SQL that retrieves Single Records (1 row per exec)..
+
+my suggestion would be that the PK index alone is sufficient ?
+
+note: check if id - count-date shows multiple dates per id.
+
+notes on lobs:
+if lobsize is <8K, the chuncksize is a waste of space+effort
 
 ****/
 
 drop table t1;
 drop table pt1;
 
-create table pt1 (
+create table t1 (
   id   number
 , dtc  date
 , Blb  blob
@@ -39,13 +50,36 @@ create table pt1 (
 , pld  varchar2(4000)
 ) ;
 
-create unique index pt1_pk on pt1 ( id, dtc ) ; 
+create unique index t1_pk on t1 ( id, dtc ) ; 
+alter table t1 add constraint t1_pk primary key ( id, dtc ) ;
 
+-- and the two funny indexes
+create index t1_idx_lvl on t1 ( id, dtc, lvl ) ;
+create index t1_idx_dtm on t1 ( id, dtc, dtm ) ;
+
+
+-- and the partitioned version
+create table pt1 (
+  id   number
+, dtc  date
+, Blb  blob
+, lvl  number
+, dtm  date
+, pld  varchar2(4000)
+)
+partition by range ( DTC ) interval (numtoyminterval(1, 'month'))
+ (partition PT1_P0 values less than (to_date(' 2021-01-01', 'YYYY-MM-DD'))
+  segment creation immediate
+  lob ( blb ) store as securefile ( disable storage in row ) 
+  ) ;
+
+-- choose local or global..
+create unique index pt1_pk on pt1 ( id, dtc ) local ; 
 alter table pt1 add constraint pt1_pk primary key ( id, dtc ) ;
 
 -- and the two funny indexes
-create index pt1_idx_lvl on pt1 ( id, dtc, lvl ) ;
-create index pt1_idx_dtm on pt1 ( id, dtc, dtm ) ;
+create index pt1_idx_lvl on pt1 ( id, dtc, lvl ) local ;
+create index pt1_idx_dtm on pt1 ( id, dtc, dtm ) local ;
 
 
 -- put some data in, try for 10K rows, to aim for 4 partitions?
@@ -56,7 +90,7 @@ create index pt1_idx_dtm on pt1 ( id, dtc, dtm ) ;
 insert into pt1
 select
    trunc ( rownum -1)                               -- sequene, id...
-,  to_date ( '2021-JAN-01', 'YYYY-MON-DD' ) + (rownum/1440)  -- dtc
+,  to_date ( '2021-JAN-01', 'YYYY-MON-DD' ) + (4*rownum/1440)  -- dtc
 ,  ''                                              -- blob, first empty
 ,  mod ( rownum-1, 10 )                             -- levels 0-10
 ,  (sysdate - rownum/1440 )                        -- some date-modified?
@@ -64,4 +98,7 @@ select
 from dual
 connect by rownum <= 40000 ;
 
+commit ;
+
+EXEC DBMS_STATS.gather_table_stats(user, 'PT1', null, 1);
 
